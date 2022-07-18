d26dbfb91e6dcd711d73526b6263e01741683a35
Sova171 <vladkuzak@gmail.com>
2022-07-18 17:25:13 +0300

    admin v3: authors index

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Author do
-  permit_params :full_name, :date_of_birth, :biography
+  permit_params :full_name, :date_of_birth, :biography, book_ids: []
+
+  index do
+    selectable_column
+    column 'Name', :full_name
+    column :date_of_birth
+    column :biography
+    column :books
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :books
+      f.input :full_name
+      f.input :date_of_birth
+      f.input :biography
+    end
+    f.actions
+  end
 end
