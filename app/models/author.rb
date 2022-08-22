91a78e9e18f0fddc8fa8189595279d65fded8668
Sova171 <vladkuzak@gmail.com>
2022-08-22 13:45:02 +0300

    little changes

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@
 
 class Author < ApplicationRecord
   include PublicActivity::Model
-  tracked owner: proc { |controller| controller.current_admin_user }
+  tracked owner: proc { |controller| controller&.current_admin_user }
 
   has_one_attached :avatar
   has_and_belongs_to_many :books
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,7 +2,7 @@
 
 User.delete_all
 
-User.new({ email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true }).save!
+User.new({ email: 'admin@example.com', password: 'password', password_confirmation: 'password' }).save!
 User.new({ email: 'vladkuzak@gmail.com', password: 'vlad1972', password_confirmation: 'vlad1972' }).save!
 User.new({ email: 'boris_soltus@gmail.com', password: 'boris_', password_confirmation: 'boris_' }).save!
 
