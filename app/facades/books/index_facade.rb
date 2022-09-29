2a26fd6cf297312cab60e1d6844f7a09cec84eaa
Sova171 <vladkuzak@gmail.com>
2022-09-29 14:52:26 +0300

    add ratings to filters

--- app/facades/books/index_facade.rb
+++ app/facades/books/index_facade.rb
@@ -3,13 +3,14 @@
 module Books
   class IndexFacade
     include Pagy::Backend
-    attr_reader :page, :category
+    attr_reader :page, :category, :rating
 
     DEFAULT_PAGE = 1
 
-    def initialize(page:, category:)
+    def initialize(page:, category:, rating:)
       @page = page.presence || DEFAULT_PAGE
       @category = category
+      @rating = rating
     end
 
     def paginate
@@ -24,6 +25,10 @@ module Books
       Book.categories
     end
 
+    def ratings
+      Rating.all
+    end
+
     private
 
     def pagination
@@ -33,9 +38,26 @@ module Books
     def all_books
       if category.presence
         Book.where(category:)
+      elsif rating.presence
+        sorted_books
       else
         Book.order(:created_at)
       end
+    end
+
+    def query_ratings
+      Book
+        .joins(:rating_books)
+        .where(rating_books: { rating_id: rating })
+        .group(:id)
+        .count
+    end
+
+    def sorted_books
+      ids = []
+      sorted = query_ratings.sort_by { |k, v| -v }
+      sorted.each { |k, v| ids.push(k) }
+      Book.where(id: ids).in_order_of(:id, ids)
     end
   end
 end
