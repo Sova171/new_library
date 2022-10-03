236c7f575f377e8060dfbc423a72f9a6175d297a
Sova171 <vladkuzak@gmail.com>
2022-10-03 10:45:22 +0300

    welcome email to new users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth', registrations: 'users/registrations' }
 
   root 'books#index'
 
